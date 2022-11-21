45a4083cc6c25c13f46111be9675b761176e596f
luiznijah <luiznijah@gmail.com>
2022-11-21 17:18:19 -0300

    resolução do exercicio 02

---
+++ exercicio2/script.js
@@ -0,0 +1,18 @@
+                   //Exercicio 2
+let a = 10 ;
+let b = 25;
+//colocando uma variavel com valor vazio para a troca entre a e b
+let c = 
+//cosole.log no a e b para mostrar o valor atual
+console.log ("O valor de a é ", a);
+console.log ("O valor de b é ", b);
+//com c=a passo o valor de a para c 
+c = a;
+//agora mudo o valor de a para b
+a = b;
+//pego o valor que guardei de a em c e mudo de c para b
+b = c;
+//console.log para mostrar o novo resultado 
+console.log ("O valor de a é ", a);
+console.log ("O valor de b é ", b);
+
